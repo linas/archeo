09509fd7628dff5f86469a87ed217491fde79336
Linas Vepštas <linasvepstas@gmail.com>
2024-12-08 13:09:01 -0600

    directory crawl

--- src/catalog/main.py
+++ src/catalog/main.py
@@ -10,11 +10,18 @@ from witness import file_witness, witness_db_open, witness_db_close
 
 hostname = "phony"
 
+def dir_witness(hostname, dirpath):
+	diter = os.scandir(dirpath)
+	for fob in diter:
+		print("yoo", fob)
+
+
 witness_db_open('file-witness.db')
 
+dir_witness(hostname, "/tmp")
 r = file_witness(hostname, "/tmp/xxx")
 print("I got fileid ", r)
-r = file_witness("funny", "/tmp/zzz")
+r = file_witness(hostname, "/tmp/zzz")
 print("I got fileid ", r)
 
 # Close the connection
