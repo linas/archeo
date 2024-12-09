5d2f389f1d03be5ca8b5ff39be88347f44ddcd13
Linas Vepštas <linasvepstas@gmail.com>
2024-12-08 18:06:01 -0600

    Attempt hello-world in flask

---
+++ src/webui/main_panel.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+#
+# main_panel.py
+#
+# Main control panel for Archeo
+#
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def bonkers_form():
+	return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def blarg_post():
+	foo = request.form['foo']
+	bar = request.form['bar']
+	if foo == 'bla' :
+		return "<h1>Binkers ...</h1>"
+	else :
+		return "<h1>Bonkers !</h1>"
+
+if __name__ == '__main__':
+	app.run()
+
