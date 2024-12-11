cf19f4ba28e4ef8aa0ac1d0483e727035790a667
Linas Vepštas <linasvepstas@gmail.com>
2024-12-10 18:29:53 -0600

    Link the content hash

--- src/webui/filename_details.py
+++ src/webui/filename_details.py
@@ -5,10 +5,10 @@
 #
 
 from flask import render_template
-from flask_table import Table, Col, DatetimeCol
+from flask_table import Table, Col, DatetimeCol, LinkCol
 
 # The dot in front of the name searches the current dir.
-from .utils import prthash
+from .utils import prthash, to_uint64
 from .query import select_filerecords
 
 # ---------------------------------------------------------------------
@@ -18,12 +18,13 @@ from .query import select_filerecords
 # protocol, domain, filepath, filename, filesize, filecreate, filexxh, frecid
 class FilenameDetailsTable(Table):
 	row = Col('')
+	hash = LinkCol('xxHash', attr='hashstr', endpoint='directory_detail',
+      url_kwargs=dict(signedhash='xxhash'))
 	domain = Col('Domain')
 	filepath = Col('File path')
 	filesize = Col('Size (bytes)')
 	filecreate = DatetimeCol('Last modified')
-	frecid = Col('Record ID')
-	hash = Col('xxHash')
+	# frecid = Col('Record ID')
 
 # Find duplicated filenames
 def show_filename_details(filename):
@@ -35,7 +36,14 @@ def show_filename_details(filename):
 		row = dict(rec)
 		rowcount += 1
 		row['row'] = rowcount
-		row['hash'] = prthash(rec['filexxh'])
+
+		# prthash is used for display on the web page and is
+		# subject to change. The hex conversion is used in the
+		# link URL GET method and must be decodable at the other
+		# end, and thus must not change on a whim.
+		row['hashstr'] = prthash(row['filexxh'])
+		row['xxhash'] = hex(to_uint64(row['filexxh']))
+
 		filelist.append(row)
 
 	ftable = FilenameDetailsTable(filelist)
