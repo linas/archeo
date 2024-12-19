8d3d4ce5fec3b0d06726da5351022b84dd593731
Linas Vepštas <linasvepstas@gmail.com>
2024-12-18 23:19:45 -0600

    Use command-line arg

--- atoms/catalog/main.py
+++ atoms/catalog/main.py
@@ -6,7 +6,14 @@
 # This crawls a file system specified in the config file,
 # and creates or updates a witness record for each file.
 
+import sys
 from walker import walk_witness
 
-walk_witness("crawler.conf")
+if 2 != len(sys.argv):
+	print("Usage: " + sys.argv[0] + " <config file>")
+	print("\tThe config file specifies which directories to crawl,")
+	print("\tand where crawl results will be stored.")
+	sys.exit(1)
 
+# Expecting crawler.conf
+walk_witness(sys.argv[1])
