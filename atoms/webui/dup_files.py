2d5aff1de5a9a5200b17b2d0ffdfab8c813c0cba
Linas Vepštas <linasvepstas@gmail.com>
2024-12-19 13:42:42 -0600

    Continue with the query abstraction

--- atoms/webui/dup_files.py
+++ atoms/webui/dup_files.py
@@ -34,10 +34,19 @@ class DupeFileTable(Table):
 	filesize = Col('Size (bytes)')
 	filedate = DatetimeCol('Last modified')
 
-# Show duplicated items.
+# Build a list of duplicated items.
 # First argument: the property that is duplicated
-# Secnd argument: min number of duplications to rise above.
-def show_duplicates(property, min_num_dups):
+# Second argument: min number of duplications to rise above.
+#
+# Returned value: a list of items having that duplicated property.
+# The returned list contains a complete description of the item, in the
+# form of a dictionary with the item properties in the dict.
+# That is, the returned value is a list of dictionaries.
+#
+# This is a query shim, translating the result of queries to the
+# AtomSpace, into python dicts that python API's are expecting to get.
+#
+def build_duplicates(property, min_num_dups):
 
 	dup_items = find_duplicates(property, min_num_dups)
 
@@ -50,7 +59,7 @@ def show_duplicates(property, min_num_dups):
 		# a given hash. Returned columns ar properties associated with
 		# that hash, including url, filesize, filedate
 		first = True
-		fresult = get_fileinfo_from_keywords(filename=item)
+		fresult = get_fileinfo_from_keywords(**{property:item})
 		for frow in fresult:
 			itemcount += 1
 			frow['row'] = itemcount
@@ -63,13 +72,16 @@ def show_duplicates(property, min_num_dups):
 			rowlist.append(frow)
 
 		# Blank line. Maybe there's some prettier way; I can't be bothered.
+		# viz. There needs to be a CSS for the displayed table, indicating
+		# a grouping break. Not sure how to convey this grouping.
 		rowlist.append(dict(row='', hashstr='', count='', url='',
 			domain='', filepath='', filename='', filesize='', filedate=''))
 
-	ftable = DupeFileTable(rowlist)
-	return render_template("file-list.html", filecount=itemcount, filetable=ftable)
+	return rowlist
 
 # Find duplicated filenames
 def show_dup_files():
 
-	return show_duplicates('filename', 2)
+	rowlist = build_duplicates('filename', 2)
+	ftable = DupeFileTable(rowlist)
+	return render_template("file-list.html", filecount=itemcount, filetable=ftable)
