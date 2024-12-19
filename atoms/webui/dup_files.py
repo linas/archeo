def6d71ec2fc20ccdf12128f7eaba1e20fa9b1b5
Linas Vepštas <linasvepstas@gmail.com>
2024-12-19 14:21:59 -0600

    Split the duplication code into two

--- atoms/webui/dup_files.py
+++ atoms/webui/dup_files.py
@@ -8,7 +8,7 @@ from flask import render_template
 from flask_table import Table, Col, DatetimeCol, LinkCol
 
 # The dot in front of the name searches the current dir.
-from .query import find_duplicates, get_fileinfo_from_keywords
+from .property_listing import build_duplicates
 
 # ---------------------------------------------------------------------
 
@@ -33,51 +33,6 @@ class DupeFileTable(Table):
 		url_kwargs=dict(filepath='filepath'))
 	filesize = Col('Size (bytes)')
 	filedate = DatetimeCol('Last modified')
-
-# Build a list of duplicated items.
-# First argument: the property that is duplicated
-# Second argument: min number of duplications to rise above.
-#
-# Returned value: a list of items having that duplicated property.
-# The returned list contains a complete description of the item, in the
-# form of a dictionary with the item properties in the dict.
-# That is, the returned value is a list of dictionaries.
-#
-# This is a query shim, translating the result of queries to the
-# AtomSpace, into python dicts that python API's are expecting to get.
-#
-def build_duplicates(property, min_num_dups):
-
-	dup_items = find_duplicates(property, min_num_dups)
-
-	itemcount = 0
-	rowlist = []
-	for item in dup_items:
-		itemcount += 1
-
-		# We use this to construct a second query, for all files with
-		# a given hash. Returned columns ar properties associated with
-		# that hash, including url, filesize, filedate
-		first = True
-		fresult = get_fileinfo_from_keywords(**{property:item})
-		for frow in fresult:
-			itemcount += 1
-			frow['row'] = itemcount
-			if first:
-				first = False
-			else :
-				frow['hashstr'] = ''
-				frow['count'] = ''
-
-			rowlist.append(frow)
-
-		# Blank line. Maybe there's some prettier way; I can't be bothered.
-		# viz. There needs to be a CSS for the displayed table, indicating
-		# a grouping break. Not sure how to convey this grouping.
-		rowlist.append(dict(row='', hashstr='', count='', url='',
-			domain='', filepath='', filename='', filesize='', filedate=''))
-
-	return rowlist
 
 # Find duplicated filenames
 def show_dup_files():
---
+++ atoms/webui/property_listing.py
@@ -0,0 +1,57 @@
+#
+# property_listing.py
+#
+# Convert Atomese item properites to python lists-of-dictionaries.
+# Each Atomese item is mapped to a python dictionary. Multiple
+# dictionaries are places into a python list (could have been a set,
+# there is no particular ordering that matters.)
+
+# The dot in front of the name searches the current dir.
+from .query import find_duplicates, get_fileinfo_from_keywords
+
+# ---------------------------------------------------------------------
+
+# Build a list of duplicated items.
+# First argument: the property that is duplicated
+# Second argument: min number of duplications to rise above.
+#
+# Returned value: a list of items having that duplicated property.
+# The returned list contains a complete description of the item, in the
+# form of a dictionary with the item properties in the dict.
+# That is, the returned value is a list of dictionaries.
+#
+# This is a query shim, translating the result of queries to the
+# AtomSpace, into python dicts that python API's are expecting to get.
+#
+def build_duplicates(property, min_num_dups):
+
+	dup_items = find_duplicates(property, min_num_dups)
+
+	itemcount = 0
+	rowlist = []
+	for item in dup_items:
+		itemcount += 1
+
+		# We use this to construct a second query, for all files with
+		# a given hash. Returned columns ar properties associated with
+		# that hash, including url, filesize, filedate
+		first = True
+		fresult = get_fileinfo_from_keywords(**{property:item})
+		for frow in fresult:
+			itemcount += 1
+			frow['row'] = itemcount
+			if first:
+				first = False
+			else :
+				frow['hashstr'] = ''
+				frow['count'] = ''
+
+			rowlist.append(frow)
+
+		# Blank line. Maybe there's some prettier way; I can't be bothered.
+		# viz. There needs to be a CSS for the displayed table, indicating
+		# a grouping break. Not sure how to convey this grouping.
+		rowlist.append(dict(row='', hashstr='', count='', url='',
+			domain='', filepath='', filename='', filesize='', filedate=''))
+
+	return rowlist
