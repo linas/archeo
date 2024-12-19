b4444c908ac22b3721e0c3b00e661d972c32bf84
Linas Vepštas <linasvepstas@gmail.com>
2024-12-19 17:09:57 -0600

    Move common code to property file

--- atoms/webui/property_listing.py
+++ atoms/webui/property_listing.py
@@ -20,6 +20,7 @@ class item_collection :
 	def __init__(self) :
 		return
 
+	# -------------------------------------------------------------------------
 	# Build a list of duplicated items.
 	# First argument: the property that is duplicated
 	# Second argument: min number of duplications to rise above.
@@ -65,3 +66,49 @@ class item_collection :
 
 		self.itemcount = itemcount
 		return rowlist
+
+	# -------------------------------------------------------------------------
+
+	# Create a list of dictionaries describing files.
+	# The list to be created is specified via the key-value arguements
+	# The resulting list is grouped together according to common hash values
+	#
+	# The  argument is a property.
+	def build_file_list(self, **kwargs) :
+
+		dentries = get_fileinfo_from_keywords(**kwargs)
+
+		# Get a list of all distinct hashes in this directory
+		hashset = set()
+		for dentry in dentries:
+			hashset.add(dentry['hashstr'])
+
+		# Gather names of the files for each hash
+		filist = []
+		itemcount = 0;
+		for hash in hashset:
+			itemcount += 1
+
+			# Get the file(s) with this hash.
+			kwargs['hashstr'] = hash
+			dentries = get_fileinfo_from_keywords(**kwargs)
+
+			nfiles = len(dentries)
+
+			difro = dentries[0]
+			difro['row'] = itemcount
+
+			filist.append(difro)
+
+			# The hash may appear more than once in this directory.
+			# That is, there may be more than one file, having a different
+			# name, but with the same contents. Group these together.
+			# Blank out the hash to avoid clutter.
+			for idx in range (1, nfiles) :
+				difro = dentries[idx]
+				difro['row'] = ''
+				difro['hashstr'] = ''
+				filist.append(difro)
+
+		self.itemcount = itemcount
+		return filist
--- atoms/webui/show_path.py
+++ atoms/webui/show_path.py
@@ -7,10 +7,7 @@
 from flask import render_template
 from flask_table import Table, Col, DatetimeCol, LinkCol, create_table
 
-from .query import get_fileinfo_from_keywords
-
-# General plan:
-# -- Print the directory contents.
+from .property_listing import item_collection
 
 # Declare table header
 class PathListTable(Table):
@@ -25,56 +22,13 @@ class PathListTable(Table):
 
 # -------------------------------------------------------------------------
 
-# Create a list of dictionaries describing files.
-# The list to be created is specified via the key-value arguements
-#
-# The  argument is a property.
-def build_file_list(**kwargs) :
-
-	dentries = get_fileinfo_from_keywords(**kwargs)
-
-	# Get a list of all distinct hashes in this directory
-	hashset = set()
-	for dentry in dentries:
-		hashset.add(dentry['hashstr'])
-
-	# Gather names of the files for each hash
-	filist = []
-	totcount = 0;
-	for hash in hashset:
-		totcount += 1
-
-		# Get the file(s) with this hash.
-		kwargs['hashstr'] = hash
-		dentries = get_fileinfo_from_keywords(**kwargs)
-
-		nfiles = len(dentries)
-
-		difro = dentries[0]
-		difro['row'] = totcount
-
-		filist.append(difro)
-
-		# The hash may appear more than once in this directory.
-		# That is, there may be more than one file, having a different
-		# name, but with the same contents. Group these together.
-		# Blank out the hash to avoid clutter.
-		for idx in range (1, nfiles) :
-			difro = dentries[idx]
-			difro['row'] = ''
-			difro['hashstr'] = ''
-			filist.append(difro)
-
-	return filist
-
-# -------------------------------------------------------------------------
-
 # Print a directory listing.
 #
 # The argument is a filepath.
 def show_path_listing(filepath) :
 
-	filist = build_file_list(filepath=filepath)
+	itco = item_collection()
+	filist = itco.build_file_list(filepath=filepath)
 
 	# Generate a detailed report of how the directories dffer
 	path_list_table = PathListTable(filist)
